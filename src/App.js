d36d4e9fdcb583d8d03f243c1186dfa52e917dcc
sara <saraelsadek55555@gmail.com>
2024-03-31 13:27:15 +0200

    first add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height="100vh"
+      projectID="29618e68-cb5a-4242-b62c-48d3c098273f"
+      userName="sara-mahmoud"
+      userSecret="123456789"
+    />
+      
   );
 }
 
